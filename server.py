cc4c86281bbb78dcc5bac67fe8b3e39dbc164399
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2018-11-17 13:42:53 +0530

    Added CORS support

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS, cross_origin
 import sqlite3
+
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origin": "http://localhost:5000/", "supports_credentials": "true"}})
 
 @app.route("/")
 def renderPage():
