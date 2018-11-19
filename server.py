b90ec8fc4dba10b589e065db990dfad5d7d59642
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2018-11-19 15:13:33 +0530

    Added addOnlineUsers() and removeOnlineUsers()

--- server.py
+++ server.py
@@ -23,5 +23,26 @@ def updateMessage():
         msg_list.append({"user":row[0], "msg":row[1], "id":row[2]})
     return jsonify({"msg_list":msg_list})
 
+@app.route("/user/add", methods=['POST', 'GET'])
+def addOnlineUsers():
+    con = sqlite3.connect("users.sqlite")
+    cur = con.cursor()
+    if request.method =="POST":
+        req = request.get_json()
+        cur.execute("INSERT INTO users(USER, ID) VALUES (?, ?)", (req["user"],req["id"]))
+        con.commit()
+    cur.execute("SELECT USER, ID FROM users")
+    users_list = list()
+    for row in cur:
+        users_list.append({"user":row[0], "id":row[1]})
+    return jsonify({"user_list": users_list})
+
+@app.route("/user/remove", methods=['POST'])
+def removeOnlineUsers():
+    con = sqlite3.connect("users.sqlite")
+    cur = con.cursor()
+    cur.execute("DELETE FROM users WHERE ID=(?)", (req["id"]))
+    con.commit()
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
