986175c67a48e401b522725931342f077c51d9f5
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2018-11-19 12:09:51 +0530

    Added support for manipulating ID of user

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import sqlite3
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origin": "http://localhost:5000/", "supports_credentials": "true"}}) #Add address of client
+cors = CORS(app, resources={r"/*": {"origin": "http://localhost:8000/", "supports_credentials": "true"}}) #Add address of client
 
 @app.route("/")
 def renderPage():
@@ -15,12 +15,12 @@ def updateMessage():
     cur = con.cursor()
     if request.method =="POST":
         req = request.get_json()
-        cur.execute("INSERT INTO msgs(USER, MESSAGE) VALUES (?, ?)", (req["user"],req["msg"]))
+        cur.execute("INSERT INTO msgs(USER, MSG, ID) VALUES (?, ?, ?)", (req["user"],req["msg"], req["id"]))
         con.commit()
-    cur.execute("SELECT USER, MESSAGE FROM msgs")
+    cur.execute("SELECT USER, MSG, ID FROM msgs")
     msg_list = list()
     for row in cur:
-        msg_list.append({"user":row[0], "msg":row[1]})
+        msg_list.append({"user":row[0], "msg":row[1], "id":row[2]})
     return jsonify({"msg_list":msg_list})
 
 if __name__ == "__main__":
