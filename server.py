cdbe1e14fa656b5823d3b9d38779ce093c101e34
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2018-11-23 20:05:33 +0530

    Added creation of dbs on start of server

--- server.py
+++ server.py
@@ -2,6 +2,27 @@ from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 import sqlite3
 
+msgdb = sqlite3.connect("msgs.sqlite")
+msgcur = msgdb.cursor()
+msgcur.executescript("""
+CREATE TABLE IF NOT EXISTS MSGS(
+SNO INTEGER PRIMARY KEY AUTOINCREMENT,
+USER TEXT NOT NULL,
+MSG TEXT NOT NULL,
+ID INTEGER NOT NULL
+);""")
+msgdb.commit()
+
+userdb = sqlite3.connect("users.sqlite")
+usercur = userdb.cursor()
+usercur.executescript("""
+CREATE TABLE IF NOT EXISTS USERS(
+SNO INTEGER PRIMARY KEY AUTOINCREMENT,
+USER TEXT NOT NULL,
+ID INTEGER NOT NULL
+);""")
+userdb.commit()
+
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origin": "http://localhost:8000/", "supports_credentials": "true"}}) #Add address of client
 
@@ -15,9 +36,9 @@ def updateMessage():
     cur = con.cursor()
     if request.method =="POST":
         req = request.get_json()
-        cur.execute("INSERT INTO msgs(USER, MSG, ID) VALUES (?, ?, ?)", (req["user"],req["msg"], req["id"]))
+        cur.execute("INSERT INTO MSGS(USER, MSG, ID) VALUES (?, ?, ?)", (req["user"],req["msg"], req["id"]))
         con.commit()
-    cur.execute("SELECT USER, MSG, ID FROM msgs")
+    cur.execute("SELECT USER, MSG, ID FROM MSGS")
     msg_list = list()
     for row in cur:
         msg_list.append({"user":row[0], "msg":row[1], "id":row[2]})
@@ -30,13 +51,13 @@ def addOnlineUsers():
     if request.method =="POST":
         req = request.get_json()
         i=0
-        cur.execute("SELECT USER, ID FROM users WHERE ID=(?)", (req["id"],))
+        cur.execute("SELECT USER, ID FROM USERS WHERE ID=(?)", (req["id"],))
         for row in cur:
             i+=1
         if i==0:
-            cur.execute("INSERT INTO users(USER, ID) VALUES (?, ?)", (req["user"],req["id"]))
+            cur.execute("INSERT INTO USERS(USER, ID) VALUES (?, ?)", (req["user"],req["id"]))
             con.commit()
-    cur.execute("SELECT USER, ID FROM users")
+    cur.execute("SELECT USER, ID FROM USERS")
     users_list = list()
     for row in cur:
         users_list.append({"user":row[0], "id":row[1]})
@@ -47,10 +68,11 @@ def removeOnlineUsers():
     req = request.get_json()
     con = sqlite3.connect("users.sqlite")
     cur = con.cursor()
-    cur.execute("DELETE FROM users WHERE ID=(?)", (req["id"],))
+    cur.execute("DELETE FROM USERS WHERE ID=(?)", (req["id"],))
     con.commit()
     return jsonify({"status":"deleted"})
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(port=8000)
+
